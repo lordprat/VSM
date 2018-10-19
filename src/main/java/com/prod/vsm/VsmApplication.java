7a35c0c7ca9637121ab0ba033b31e63cc269a25a
lordprat <44286985+lordprat@users.noreply.github.com>
2018-10-19 13:39:55 +0530

    Initial commit

---
+++ src/main/java/com/prod/vsm/VsmApplication.java
@@ -0,0 +1,13 @@
+package com.prod.vsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsmApplication.class, args);
+		System.out.println("Welcome");
+	}
+}
